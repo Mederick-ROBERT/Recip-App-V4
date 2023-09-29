f2991b7eea55dc9545ceaf2e8c84f784a6e1c1c0
Mederick-ROBERT <mederick.robert@free.fr>
2023-09-29 18:44:55 +0200

    Test Hello World

---
+++ Recip-App-Front/pages/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Index</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    mounted() {
+
+    }
+}
+</script>
+
+<style lang='scss'>
+
+</style>
